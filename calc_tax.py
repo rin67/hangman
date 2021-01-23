485c7051f2c9dd595ec6f8f22dac36eb6d09df65
yukari <rinpom56@gmail.com>
2021-01-23 13:27:18 +0900

    my first commit

---
+++ calc_tax.py
@@ -0,0 +1,11 @@
+"""
+calc_tax関数は本体価格と消費税率を引数として
+受け取り、消費税率を返します
+"""
+def calc_tax(price,rate):
+    tax = price * rate / 100
+    #int関数を使って整数を返す
+    return int(tax)
+
+a = calc_tax(1249,10)
+print(a)
